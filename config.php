5d3674d16039c1f9a40be25910517df6e02233c4
heetgala <49522408+heetgala@users.noreply.github.com>
2024-04-29 15:50:46 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $con = new PDO("sqlsrv:server = cmpro.database.windows.net; Database = cmproject", "Cloud", "cmpro@008");
+    $con = new PDO("sqlsrv:server = cmpro.database.windows.net; Database = cloud", "Cloud", "cmpro@008");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     print("Error connecting to SQL Server.");
